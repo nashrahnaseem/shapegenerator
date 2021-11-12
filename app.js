d8faeeb26c16ae7186eb854888224abad60223a2
Nashrah Naseem <54800516+nashrahnaseem@users.noreply.github.com>
2021-11-12 19:11:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-let button = document.getElementById("button");
-var numberInTheShape = 1;
-var oldValue = 0;
+let button=document.getElementById("button");
+var numberInTheShape=1;
+var oldValue=0;
+button.onclick=()=>{
+    let n=document.getElementById("number").value;
+    const circle=document.getElementById("circle");
+    const square=document.getElementById("square");
+    const rectangle=document.getElementById("rectangle");
+    const ellipse=document.getElementById("ellipse");
+    const $box =document.getElementById("box");
+   
+    n=Number(oldValue)+Number(n);
+    for(let j=numberInTheShape;j<=n;j++){
+        var shape=document.createElement("div");
+        shape.innerHTML+=numberInTheShape;
+        if(square.checked){
+            shape.classList.add("square");
+        }
+        else if(rectangle.checked){
+            shape.classList.add("rectangle");
+        }
+        else if(ellipse.checked){
+            shape.classList.add("ellipse");
+        }
+         else if(circle.checked){
+            shape.classList.add("circle");
+        }
+        else{
+            document.write("invalid input");
+        }
+        $box.appendChild(shape);
+        numberInTheShape++;
+        oldValue=document.getElementById("box").lastElementChild.innerHTML;
 
-button.onclick = () => {
-  let n = document.getElementById("number").value;
-  const circle = document.getElementById("circle");
-  const square = document.getElementById("square");
-  const rect = document.getElementById("rectangle");
-  const $box = document.getElementById("box");
-  let j;
-  // console.log(choice)
-  n = Number(oldValue) + Number(n);
-  // console.log(n);
-  for (let j = numberInTheShape; j <= n; j++) {
-    var shape = document.createElement("div");
-    shape.innerHTML += numberInTheShape;
-    if (square.checked) {
-      shape.classList.add("square");
-    } else if (circle.checked) {
-      shape.classList.add("circle");
-    } else if (rect.checked) {
-      shape.classList.add("rectangle");
-    } else {
-      document.write("invalid Input");
     }
-    $box.appendChild(shape);
-    numberInTheShape++;
-    oldValue = document.getElementById("box").lastElementChild.innerHTML;
-  }
-};
+}
+//    <div class="square">   1  </div>
